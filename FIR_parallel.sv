b432479c3e8b69fdb52c705b845cffb893500fc1
George <georgelin469@gmail.com>
2023-12-26 18:32:32 -0700

    Adder tree architecture

--- FIR_parallel.sv
+++ FIR_parallel.sv
@@ -5,17 +5,16 @@ module FIR_adderTree  (
     output [31:0] output_signal_y);
     
     parameter order = 64;
-    parameter int FIRfilterCoeffs [order] = 
+    int FIRfilterCoeffs [order] = 
     '{-764, -965, -1236, -1588, -2004, -2443, -2832, -3071, -3041, -2608, -1637, 0, 2405, 5646, 9747, 14676, 20348, 26618, 33288, 40120, 46842, 53167, 58813, 63516, 67051, 69245, 69989, 69245, 67051, 63516, 58813, 53167, 46842, 40120, 33288, 26618, 20348, 14676, 9747, 5646, 2405, 0, -1637, -2608, -3041, -3071, -2832, -2443, -2004, -1588, -1236, -965, -764, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
     reg [31:0] delayLine [order-1:0];
     reg [31:0] multiplyResult [order-1:0];  // 2D array used to score multiply results
-    reg [31:0] layer0Acc [order/1-1:0];
-    reg [31:0] layer1Acc [order/2-1:0];
-    reg [31:0] layer2Acc [order/4-1:0];
-    reg [31:0] layer3Acc [order/8-1:0];
-    reg [31:0] layer4Acc [order/16-1:0];
-    reg [31:0] layer5Acc [order/32-1:0];
+    reg [31:0] layer0Acc [order/2-1:0];
+    reg [31:0] layer1Acc [order/4-1:0];
+    reg [31:0] layer2Acc [order/8-1:0];
+    reg [31:0] layer3Acc [order/16-1:0];
+    reg [31:0] layer4Acc [order/32-1:0];
 
     // must be zero padded first
     
@@ -25,7 +24,7 @@ module FIR_adderTree  (
 
     // Reseting regisers 
     
-    always_ff @( posedge clk ) begin : FIR_update_buffer_delay_line_with_zero_init
+    always_ff @( posedge clk ) begin 
         if (reset) begin
             for (i = 0; i < order; i++) begin
                 delayLine[i] <= 0;
@@ -49,26 +48,23 @@ module FIR_adderTree  (
     // Combination stage
     
     always_comb begin
-        for (i0 = 0; i0 < order/2; i0=i0+2) begin 
-            layer0Acc[i0] <= multiplyResult[i0]+multiplyResult[i0+1];
+        for (i0 = 0; i0 < order; i0=i0+2) begin 
+            layer0Acc[i0/2] <= multiplyResult[i0]+multiplyResult[i0+1];
         end
-        for (i1 = 0; i1 < order/4; i1=i1+2) begin
-            layer1Acc[i1] <= layer0Acc[i1] + layer0Acc[i1+1];
+        for (i1 = 0; i1 < order/2; i1=i1+2) begin
+            layer1Acc[i1/2] <= layer0Acc[i1] + layer0Acc[i1+1];
         end
-        for (i2 = 0; i2 < order/8; i2=i2+2) begin
-            layer2Acc[i2] <= layer1Acc[i2] + layer1Acc[i2+1];
+        for (i2 = 0; i2 < order/4; i2=i2+2) begin
+            layer2Acc[i2/2] <= layer1Acc[i2] + layer1Acc[i2+1];
         end
-        for (i3 = 0; i3 < order/16; i3=i3+2) begin
-            layer3Acc[i3] <= layer2Acc[i3] + layer2Acc[i3+1];
+        for (i3 = 0; i3 < order/8; i3=i3+2) begin
+            layer3Acc[i3/2] <= layer2Acc[i3] + layer2Acc[i3+1];
         end
-        for (i4 = 0; i4 < order/32; i4=i4+2) begin
-            layer4Acc[i4] <= layer3Acc[i4] + layer3Acc[i4+1];
-        end
-        for (i5 = 0; i5 < order/64; i5=i5+2) begin
-            layer5Acc[i5] <= layer4Acc[i5] + layer4Acc[i5+1];
+        for (i4 = 0; i4 < order/16; i4=i4+2) begin
+            layer4Acc[i4/2] <= layer3Acc[i4] + layer3Acc[i4+1];
         end
     end
 
-    assign output_signal_y = layer5Acc[1] + layer5Acc[0];
+    assign output_signal_y = layer4Acc[1] + layer4Acc[0];
 
 endmodule
--- adderTreeTest.do
+++ adderTreeTest.do
@@ -1,9 +1,11 @@
 onerror {resume}
 quietly WaveActivateNextPane {} 0
-add wave -noupdate -expand -group tb_signal -format Analog-Step -height 74 -max 2808.0 -min -2808.0 /tb_FIR_directForm/signal
-add wave -noupdate -format Analog-Step -height 74 -max 580998494.99999988 -min -519496589.0 /tb_FIR_directForm/DUT/output_signal_y
+add wave -noupdate -group tb_signal -format Analog-Step -height 74 -max 2808.0 -min -2808.0 /tb_FIR_directForm/signal
+add wave -noupdate /tb_FIR_directForm/DUT/multiplyResult
+add wave -noupdate -expand -group {input signal} -format Analog-Step -height 74 -max 2808.0 -min -2808.0 /tb_FIR_directForm/DUT/incoming_signal_x
+add wave -noupdate -format Analog-Step -height 74 -max 580998000.00000012 -min -519497000.0 /tb_FIR_directForm/DUT/output_signal_y
 TreeUpdate [SetDefaultTree]
-WaveRestoreCursors {{Cursor 1} {405 ps} 0}
+WaveRestoreCursors {{Cursor 1} {745 ps} 0}
 quietly wave cursor active 1
 configure wave -namecolwidth 150
 configure wave -valuecolwidth 293
@@ -19,4 +21,4 @@ configure wave -griddelta 40
 configure wave -timeline 0
 configure wave -timelineunits ps
 update
-WaveRestoreZoom {0 ps} {2520 ps}
+WaveRestoreZoom {0 ps} {2270 ps}
